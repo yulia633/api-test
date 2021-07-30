ad229ead3896ca792f0ffddf3aa8946e0b6257b8
yulia633 <ignat633yulia@yandex.ru>
2021-07-30 17:18:13 +0300

    add controllers

---
+++ src/App/Controller/AdController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Controller\BaseController;
+use Slim\Http\ServerRequest;
+use Slim\Http\Response;
+
+class AdController extends BaseController
+{
+    public function getAllAds(ServerRequest $request, Response $response): Response
+    {
+        //
+    }
+
+    public function getAd(ServerRequest $request, Response $response, $args): Response
+    {
+        //
+    }
+
+    public function addAd(ServerRequest $request, Response $response): Response
+    {
+        //
+    }
+
+
+}
---
+++ src/App/Controller/BaseController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Psr\Container\ContainerInterface;
+use Slim\Http\Response;
+
+abstract class BaseController
+{
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    protected function response(Response $response, array $responseData, int $code)
+    {
+        $response->getBody()->write(json_encode($responseData, JSON_UNESCAPED_UNICODE));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus($code);
+    }
+}
